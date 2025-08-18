6bb4ef6bdc92da0f1cc4bb654c4c168d2914cf3b
tar7nic <154546879+tar7nic@users.noreply.github.com>
2025-08-18 23:17:49 +0530

    Add files via upload

---
+++ 8. FormswFlask/1/4/Exercise.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, flash, session, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+app = Flask(__name__, template_folder = r"C:\Users\Asus\OneDrive\Documents\VSC\output\.vscode\Flask\8. FormsWFlask\4")
+
+app.config['SECRET_KEY'] = 'mykey'
+
+class SimpleForm(FlaskForm):
+    breed = StringField('What breed are you?')
+    submit = SubmitField('Click Me')
+    
+@app.route('/', methods = ['GET','POST'])
+def index():
+    form = SimpleForm()
+    
+    if form.validate_on_submit():
+        session['breed'] = form.breed.data
+        flash(f"You just changed your breed to: {session['breed']}")
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form)
+    
+if __name__ == '__main__':
+    app.run(debug= True)
