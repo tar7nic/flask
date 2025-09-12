30d5a6586f40c688cac431cc3eee3da5a1a160b0
tar7nic <154546879+tar7nic@users.noreply.github.com>
2025-09-12 22:40:42 +0530

    Add files via upload

---
+++ 9. SQLwFlask/app.py
@@ -0,0 +1,61 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Asus/VSCode/Flask/9. SQLwFlask/data.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+class Puppy(db.Model):
+    __tablename__ = 'puppies'
+    
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.Text)
+    #one to many rship - many toys for 1 dog
+    toys = db.relationship('Toy',backref = 'puppy',lazy='dynamic')
+    #one to one - one puppy to one owner
+    owner = db.relationship('Owner',backref='dog',uselist=False) 
+    
+    def __init__(self,name):
+        self.name = name
+    def __repr__(self):
+        if self.owner:
+            return f"Puppy name is {self.name} and owner is{self.owner.name}"
+        else:
+            return f"Puppy name is {self.name} and has no owner yet"  
+    
+    def report_toys(self):
+        print("Here are my toys")
+        for toy in self.toys:
+             print(toy.item_name)
+        
+class Toy(db.Model):
+    __tablename__ = 'toys'
+    
+    id = db.Column(db.Integer,primary_key=True)
+    item_name = db.Column(db.Text)
+    puppy_id = db.Column(db.Integer,db.ForeignKey('puppies.id'))
+
+    def __init__(self,item_name,puppy_id):
+        self.item_name = item_name
+        self.puppy_id = puppy_id
+    
+class Owner(db.Model):
+    __tablename__ = 'owners'
+    
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.Text)
+    
+    puppy_id = db.column(db.Integer,db.ForeignKey('puppies.id'))
+    
+    def __init__(self,name,puppy_id):
+        self.name = name
+        self.puppy_id = puppy_id
+        
